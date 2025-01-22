e38a73295218c72402012ffd814dea798792fa89
JonathanPicques <jonathan.picques@gmail.com>
2025-01-22 14:04:40 +0100

    chore: upgrade to capacitor 7

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "Les3devCapacitorWebview",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "Les3devCapacitorWebview",
             targets: ["WebviewPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
     ],
     targets: [
         .target(
